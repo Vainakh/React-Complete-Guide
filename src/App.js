3c1370b6488ba7b7fc316c1d38c5ed42a50a845f
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-09-17 16:41:48 -0700

    Finish working with props

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
       <div className="App">
         <h1>Hello World!</h1>
         <p>This is really working!</p>
-        <Person/>
-        <Person/>
-        <Person/>
+        <Person name="Adlan" age="43"/>
+        <Person name="Jay" age="33">My Hobbies: Being beautiful and having babies with Adlan!</Person>
+        <Person name="David" age="30"/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const Person = () => {
-  return <p>I am a person and I am {Math.floor(Math.random() * 30)} years old!!</p>
+const Person = (props) => {
+  console.log(props)
+  return <p>I am {props.name} and I am {props.age} years old!!</p>
 };
 
 
