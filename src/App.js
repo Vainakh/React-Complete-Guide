efa8260973e9398f1e97ef12b7075338cc50cfe7
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-09-18 14:06:57 -0700

    Finish working with inline styling

--- src/App.js
+++ src/App.js
@@ -35,11 +35,23 @@ nameChangeHandler = (event) => {
 };
 
   render () {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
         <div className="App">
         <h1>Hello World!</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler("Magnificent Fiasco")}>Switch Name</button>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler("Magnificent Fiasco")}>Switch Name
+        </button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}>My Hobbies: Coding
