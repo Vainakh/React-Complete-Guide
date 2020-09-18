f9ba6a69108de9ca5ae31238436d97a83254dba8
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-09-17 17:26:55 -0700

    Revert "Finish understanding the children"
    This reverts commit a3a8f49abbbe978edb3a28886f398bf1a6f3f1cf.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,12 +2,7 @@ import React from 'react';
 
 const Person = (props) => {
   console.log(props)
-  return (
-    <div>
-      <p>I am {props.name} and I am {props.age} years old!!</p>
-      <p>{props.children}</p>
-    </div>
-  )
+  return <p>I am {props.name} and I am {props.age} years old!!</p>
 };
 
 
