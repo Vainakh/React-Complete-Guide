a3a8f49abbbe978edb3a28886f398bf1a6f3f1cf
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-09-17 17:24:52 -0700

    Finish understanding the children

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Person = (props) => {
   console.log(props)
-  return <p>I am {props.name} and I am {props.age} years old!!</p>
+  return (
+    <div>
+      <p>I am {props.name} and I am {props.age} years old!!</p>
+      <p>{props.children}</p>
+    </div>
+  )
 };
 
 
