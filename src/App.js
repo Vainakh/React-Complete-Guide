e7a0e3e44c066aa480de47d209c398b6f6e123b9
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-09-24 09:06:20 -0700

    Lists & Keys

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Person from './Person/Person';
 class App extends React.Component {
   state = {
     persons: [
-      { name: "Adlan", age: 43 },
-      { name: "Jay", age: 33 },
-      { name: "David", age: 30},
+      { id: "1", name: "Adlan", age: 43 },
+      { id: "2", name: "Jay", age: 33 },
+      { id: "3", name: "David", age: 30},
     ],
     otherState: "some other value",
     showPersons: false
@@ -66,7 +66,8 @@ togglePersonsHandler = () => {
             return <Person
             click={() => this.deletePersonHandler(index)} 
             name={person.name} 
-            age={person.age} 
+            age={person.age}
+            key={person.id} 
             />
           })}
         </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Person.css';
 
 const Person = (props) => {
-  console.log(props)
+  // console.log(props)
   return (
     <div className="Person">
       <p onClick={props.click}>I am {props.name} and I am {props.age} years old!!</p>
